5da3c71c92607cf993aece5c7c6ffac882bba80a
fajarfadillahh <fajaragustian979@gmail.com>
2022-03-10 23:02:46 +0700

    setup project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Responsive Travel Website 🌊
+
+## [Watch full on youtube Bedimcode](https://www.youtube.com/watch?v=YzRDHxbw1RU&feature=youtu.be)
+
+This website was created following a tutorial from [Bedimcode](https://www.youtube.com/c/Bedimcode). Don't forget to subscribe to the channel and turn on the notification so you don't miss the latest video from [Bedimcode](https://www.youtube.com/c/Bedimcode).
+
+![Travel](./img/preview.png)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,37 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "767px",
+      lg: "968px",
+      xl: "1024px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      green: "#11414b",
+      grey: "#44686f",
+    },
+    fontFamily: {
+      body: ["Open Sans", "sans-serif"],
+    },
+    fontWeight: {
+      reguler: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+    },
+    lineHeight: {
+      title: "150%",
+      text: "170%",
+    },
   },
   plugins: [],
 };
