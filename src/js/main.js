e20cd3c0b07823c56e663de2ff7cba4b59055d55
fajarfadillahh <fajaragustian979@gmail.com>
2022-03-11 11:29:50 +0700

    discover section

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,20 @@ function stickyHeader() {
     : header.classList.remove("sticky-header");
 }
 window.addEventListener("scroll", stickyHeader);
+
+// ===== SWIPER DISCOVER =====
+let swiper = new Swiper(".discover-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 0,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
